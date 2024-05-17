5ffe6d9cc8fac5b28f846268cbc9a7172f6089d3
DuongNguyen999 <165481404+DuongNguyen999@users.noreply.github.com>
2024-05-18 00:24:00 +0900

    Add
    Add

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double rate = 23000; // Tỉ giá USD/VND
+        double usdAmount;
+
+        // Bước 3: Nhập giá trị USD từ người dùng
+        Console.WriteLine("Nhap so tien USD:");
+        string input = Console.ReadLine();
+
+        // Kiểm tra xem giá trị nhập vào có phải là số không
+        if (!double.TryParse(input, out usdAmount))
+        {
+            Console.WriteLine("Hay nhap so tien hop le.");
+            return;
+        }
+
+        // Bước 4: Tính và hiển thị giá trị VND
+        double vndAmount = usdAmount * rate;
+        Console.WriteLine("Gia tri VND: " + vndAmount.ToString("N0") + " VND");
+    }
+}
+
+
